6a2d0995af6cc6571e7046230b5c09ac8f5ab6a8
Victor-Murilo <victor2706.vm@gmail.com>
2022-09-03 12:58:22 -0300

    Estilização completa

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ const btnSend = document.querySelector('#submit-btn');
 checkAgreement.addEventListener('change', () => {
   if (checkAgreement.checked) {
     btnSend.disabled = false;
+  } else {
+    btnSend.disabled = true;
   }
 });
 
@@ -31,65 +33,61 @@ textArea.addEventListener('keyup', () => {
   counter.innerText = 500 - textArea.value.length;
 });
 
-const salvado = {};
-const conteudos = [];
+const savedInfoObj = {};
 
-function salvarNota() {
-  for (
-    let index = 0;
-    index < document.getElementsByName('rate').length;
-    index += 1
-  ) {
-    if (document.getElementsByName('rate')[index].checked === true) {
-      salvado.avaliacao = document.getElementsByName('rate')[index].value;
+function saveRate() {
+  const rateRadios = document.getElementsByName('rate');
+
+  for (let index = 0; index < rateRadios.length; index += 1) {
+    if (rateRadios[index].checked === true) {
+      savedInfoObj.rate = rateRadios[index].value;
     }
   }
 }
 
-function salvarFamilia() {
-  for (
-    let index = 0;
-    index < document.getElementsByName('family').length;
-    index += 1
-  ) {
-    if (document.getElementsByName('family')[index].checked === true) {
-      salvado.familia = document.getElementsByName('family')[index].value;
+function saveFamily() {
+  const familyRadios = document.getElementsByName('family');
+  for (let index = 0; index < familyRadios.length; index += 1) {
+    if (familyRadios[index].checked === true) {
+      savedInfoObj.family = familyRadios[index].value;
     }
   }
 }
 
-function salvarMaterias() {
-  for (
-    let index = 0;
-    index < document.querySelectorAll('.subject').length;
-    index += 1
-  ) {
-    if (document.querySelectorAll('.subject')[index].checked === true) {
-      conteudos.push(document.querySelectorAll('.subject')[index].value);
+function saveContent() {
+  const contentsArray = [];
+  const contentsCheckbox = document.querySelectorAll('.subject');
+  for (let index = 0; index < contentsCheckbox.length; index += 1) {
+    if (contentsCheckbox[index].checked === true) {
+      contentsArray.push(contentsCheckbox[index].value);
     }
-    salvado.aprender = conteudos;
+    savedInfoObj.contents = contentsArray;
   }
 }
 
-function salvarResto() {
-  salvado.nome = document.querySelector('#input-name').value;
-  salvado.sobrenome = document.querySelector('#input-lastname').value;
-  salvado.email = document.querySelector('#input-email').value;
-  salvado.casa = document.querySelector('#house').value;
-  salvado.comentario = textArea.value;
+function saveBasicInfoAndComment() {
+  savedInfoObj.name = document.querySelector('#input-name').value;
+  savedInfoObj.lastName = document.querySelector('#input-lastname').value;
+  savedInfoObj.email = document.querySelector('#input-email').value;
+  savedInfoObj.house = document.querySelector('#house').value;
+  savedInfoObj.comment = textArea.value;
 }
 
 btnSend.addEventListener('click', () => {
   const form = document.querySelector('#evaluation-form');
-  salvarResto();
-  salvarNota();
-  salvarFamilia();
-  salvarMaterias();
-  form.innerText = `Nome: ${`${salvado.nome} ${salvado.sobrenome}`}
-  Email: ${salvado.email}
-  Casa: ${salvado.casa}
-  Família: ${salvado.familia}
-  Matérias: ${salvado.aprender.join(', ')}
-  Avaliação: ${salvado.avaliacao}
-  Observações: ${salvado.comentario}`;
+  saveBasicInfoAndComment();
+  saveRate();
+  saveFamily();
+  saveContent();
+  form.innerHTML = '';
+  const div = document.createElement('div');
+  div.className = 'savedInfoContainer';
+  div.innerHTML = `<p>Nome: ${`${savedInfoObj.name} ${savedInfoObj.lastName}`}</p>
+  <p>Email: ${savedInfoObj.email}</p>
+  <p>Casa: ${savedInfoObj.house}</p>
+  <p>Família: ${savedInfoObj.family}</p>
+  <p>Matérias: ${savedInfoObj.contents.join(', ')}</p>
+  <p>Avaliação: ${savedInfoObj.rate}</p>
+  <p>Observações: ${savedInfoObj.comment}</p>`;
+  form.appendChild(div);
 });
